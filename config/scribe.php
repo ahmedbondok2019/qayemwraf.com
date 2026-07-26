7656cfa970f50f3b0327e480d6643be7516c185e
ahmed samy <ahmedbondok2006@gmail.com>
2026-07-26 11:09:02 +0300

    feat: add Scribe documentation configuration file

--- config/scribe.php
+++ config/scribe.php
@@ -24,7 +24,7 @@ return [
 
     // The base URL displayed in the docs.
     // If you're using `laravel` type, you can set this to a dynamic string, like '{{ config("app.tenant_url") }}' to get a dynamic base URL.
-    'base_url' => 'http://127.0.0.1:8000',
+    'base_url' => env('APP_URL', 'https://egimedical.com'),
 
     // Routes to include in the docs
     'routes' => [
